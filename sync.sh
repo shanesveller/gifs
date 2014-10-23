bf538cce31bb9d21df1f01a8924a879fc0a3a3fb
Shane Sveller <shanesveller@gmail.com>
2014-10-23 13:41:18 -0500

    New sync code with S3 support

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RSYNC_OPTS="-avPhzc --delete-after"
+RSYNC_OPTS="-avPhzc --delete-after --prune-empty-dirs"
 RSYNC_HOST=root@sys
 RSYNC_FOLDER=/var/www/gifs.shsv.pics
 
@@ -8,3 +8,4 @@ jekyll build && \
 rm -rf _site && \
 mv build _site && \
 rsync $RSYNC_OPTS _site/ $RSYNC_HOST:$RSYNC_FOLDER
+aws s3 sync --acl public-read --delete --profile $S3_PROFILE --recursive --size-only _site/ $S3_BUCKET
