ded86537b0d8787094fe9217c2b936333b0fbb51
Shane Sveller <shanesveller@gmail.com>
2015-03-18 12:27:44 -0500

    New gifs 20150318

--- sync.sh
+++ sync.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 RSYNC_OPTS="-avPhzc --delete-after --prune-empty-dirs"
-RSYNC_HOST=root@sys
+RSYNC_HOST=root@vps
 RSYNC_FOLDER=/var/www/gifs.shsv.pics
 
 ./script/build_site_index && \
 jekyll build && \
-rm -rf _site && \
-mv build _site && \
 rsync $RSYNC_OPTS _site/ $RSYNC_HOST:$RSYNC_FOLDER
 aws s3 sync --acl public-read --delete --profile $S3_PROFILE --recursive --size-only _site/ $S3_BUCKET
