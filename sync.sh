c5dfbcb9315d731def314af6cf303a28f28cd01c
Shane Sveller <shanesveller@gmail.com>
2015-06-03 14:55:38 -0500

    More new gifs

--- sync.sh
+++ sync.sh
@@ -6,4 +6,4 @@ RSYNC_FOLDER=/var/www/gifs.shsv.pics
 ./script/build_site_index && \
 jekyll build && \
 rsync $RSYNC_OPTS _site/ $RSYNC_HOST:$RSYNC_FOLDER
-aws s3 sync --acl public-read --delete --profile $S3_PROFILE --recursive --size-only _site/ $S3_BUCKET
+aws s3 sync --acl public-read --delete --profile $S3_PROFILE --size-only _site/ $S3_BUCKET
