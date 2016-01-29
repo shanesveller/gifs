7266428b7b849bcebed7e15e0e1b278527cb2536
Shane Sveller <shanesveller@gmail.com>
2016-01-29 11:42:41 -0600

    No more rsync

--- sync.sh
+++ sync.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-RSYNC_OPTS="-avPhzc --delete-after --prune-empty-dirs"
-RSYNC_HOST=root@vps
-RSYNC_FOLDER=/var/www/gifs.shsv.pics
-
 ./script/build_site_index && \
-jekyll build && \
-rsync $RSYNC_OPTS _site/ $RSYNC_HOST:$RSYNC_FOLDER
+jekyll build --incremental && \
 aws s3 sync --acl public-read --delete --profile $S3_PROFILE --size-only _site/ $S3_BUCKET
